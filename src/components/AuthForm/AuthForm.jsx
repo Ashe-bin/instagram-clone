02159bc3e64007379c86263fa43b97589da50912
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2025-02-07 14:50:11 +0300

    fix: login and signup

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,8 +1,7 @@
-import { Box, Flex, Text, VStack } from "@chakra-ui/react";
+import { Box, Flex, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 import Login from "./Login";
 import Signup from "./Signup";
-import GoogleAuth from "./GoogleAuth";
 import { InstagramLogo } from "../../assets/constants";
 
 const AuthForm = () => {
@@ -14,25 +13,12 @@ const AuthForm = () => {
         width={"full"}
         border={"1px solid gray"}
         borderRadius={10}
-        padding={5}
+        paddingY={10}
+        paddingX={5}
       >
-        <VStack spacing={4} mx={"auto"}>
+        <VStack spacing={5} mx={"auto"}>
           <InstagramLogo />
           {isLogin ? <Login /> : <Signup />}
-          <Flex
-            alignItems={"center"}
-            justifyContent={"center"}
-            my={4}
-            gap={1}
-            w={"full"}
-          >
-            <Box flex={2} h={"1px"} bg={"gray.400"} />
-            <Text mx={1} color={"white"}>
-              OR
-            </Text>
-            <Box flex={2} h={"1px"} bg={"gray.400"} />
-          </Flex>
-          <GoogleAuth prefix={isLogin ? "Login" : "Signup"} />
         </VStack>
       </Box>
       <Box border={"1px solid gray"} borderRadius={4} padding={5}>
